711966b50a5d82d9520ea23fbee8062d09d13de2
GuysRGithub <guysdeveloperrobot@gmail.com>
2022-10-20 08:23:45 +0700

    Create contact listing page

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props: {
+        contact: { type: Object, required: true },
+    },
+};
+</script>
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Name:</strong>
+            {{ contact.name }}
+
+        </div>
+        <div class="p-1">
+            <strong>E-mail:</strong>
+            {{ contact.email }}
+        </div>
+        <div class="p-1">
+            <strong>Address:</strong>
+            {{ contact.address }}
+        </div>
+        <div class="p-1">
+            <strong>Phone:</strong>
+            {{ contact.phone }}
+        </div>
+        <div class="p-1">
+            <strong>Favorite:&nbsp;</strong>
+            <i v-if="contact.favorite" class="fas fa-check" />
+            <i v-else class="fas fa-times" />
+        </div>
+    </div>
+</template>
