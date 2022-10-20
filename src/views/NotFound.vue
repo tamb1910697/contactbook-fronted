b933b3420a6a6efe239a04fcafd9c8bf33375ead
GuysRGithub <guysdeveloperrobot@gmail.com>
2022-10-20 08:26:29 +0700

    Implement the 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, Page not found. Return
+            <router-link to="/">
+                home.
+            </router-link>
+        </p>
+    </div>
+</template>
